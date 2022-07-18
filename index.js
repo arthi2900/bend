33e411368c40ba88fed64c93085354712f6a2b6d
arthi2900 <aaarthi2927@gmail.com>
2022-07-19 01:03:41 +0530

    add

--- index.js
+++ index.js
@@ -30,17 +30,16 @@ mongoose.connect(process.env.MONGO_URL, {
   .catch((err) => console.log(err));
 
 const storage = multer.diskStorage({
-  destination: (req,file, cb) => {
+  destination: (req, file, cb) => {
     cb(null, "images");
   },
-  filename: (req,file, cb) => {
+  filename: (req, file, cb) => {
     cb(null, req.body.name);
   },
 });
 
 const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"),
-(req, res) => {
+app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
