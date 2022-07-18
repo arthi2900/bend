74c3e8beb1d9375a6f5ece24a692b250f22d8646
arthi2900 <aaarthi2927@gmail.com>
2022-07-18 23:10:33 +0530

    addd

--- index.js
+++ index.js
@@ -18,18 +18,16 @@ app.use(
 );
 
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "/images")));
-
+app.use("/images",express.static(path.join(__dirname,"/images")))
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify:true
   })
-  .then(console.log("Connected to MongoDB"))
+.then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
-
-const storage = multer.diskStorage({
+   const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "images");
   },
@@ -39,10 +37,9 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
+app.post("api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
-
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
